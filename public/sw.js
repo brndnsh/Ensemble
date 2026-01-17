e84b1f3e95289da5a2a3a7120e3136a98e03ef59
Brandon Shea <brandon@debian-desktop.home.lan>
2026-01-16 23:03:26 -0500

    chore: bump version to 2.21

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Note: Keep CACHE_NAME version in sync with APP_VERSION in config.js
-const CACHE_NAME = 'ensemble-v2.20';
+const CACHE_NAME = 'ensemble-v2.21';
 const ASSETS = [
     './',
     './index.html',
