c29cd6bf5b03454e997416b906428518c1cb07ff
Brandon Shea <brandon@debian-desktop.home.lan>
2026-01-17 21:02:04 -0500

    chore: increment sw version to 2.28

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Note: Keep CACHE_NAME version in sync with APP_VERSION in config.js
-const CACHE_NAME = 'ensemble-v2.27';
+const CACHE_NAME = 'ensemble-v2.28';
 const ASSETS = [
     './',
     './index.html',
